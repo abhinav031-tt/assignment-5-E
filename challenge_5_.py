7b306b13beca01f1b65e878c5e4fe2dabd8e1640
abhinav031-tt <113112279+abhinav031-tt@users.noreply.github.com>
2022-10-13 23:53:51 +0530

    Add files via upload

---
+++ challenge_5_.py
@@ -0,0 +1,48 @@
+# Handling a Bank Account
+
+
+class Account:
+    def __init__(self):
+        self.balance = 0
+        print("The Account is created")
+
+    def deposit(self,amount):
+        amount = float(input("enter the amount to be deposit : "))
+        self.balance = self.balance + amount
+        print("Deposit is successful and the balance in the account is : ",self.balance )
+
+    def withdrawal(self,amount):
+        amount = float(input("enter the amount to be withdrawal : "))
+        if (self.balance >= amount):
+            self.balance = self.balance - amount
+            print ("The withdrawal is successfull and balance id : ",self.balance)
+        else:
+            print("insuficient fund ")
+
+
+    def getBalance(self):
+        print("Balance in the account is %f"% self.balance)
+
+
+class SavingAccount(Account):
+    def _init_(self,balance,interestrate):
+        super()._init_(balance)
+        self.interestRate = interestrate
+
+    def interestrate(self,interestrate,p=0,t=0,r=0):
+        p = float(input("enter the balance in account :"))
+        t = int(input("enter the time period to keep :"))
+        r = float(input("enter the interestrate :"))
+        print("The principle amount is :",p)
+        print("The time period in year is :", t)
+        print("The rate of interest : ",r)
+
+        interestrate = (p * t * r)/100
+        print("interestamount is :",interestrate)
+        pass
+Account_obj = Account()
+Account_obj.deposit() 
+Account_obj.withdrawal()
+Account_obj.getBalance()
+SavingAccount_obj=SavingAccount(balance=0,interestrate=0)
+SavingAccount_obj.interestrate()

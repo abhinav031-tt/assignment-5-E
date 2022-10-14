38814bc33640e29d3e4ad005d60af24c2a429dd3
abhinav031-tt <113112279+abhinav031-tt@users.noreply.github.com>
2022-10-14 20:29:25 +0530

    Update challenge_5_.py

--- challenge_5_.py
+++ challenge_5_.py
@@ -2,18 +2,17 @@
 
 
 class Account:
-    def __init__(self,interestrate):
+    def __init__(self):
         self.balance = 0
-        self.interestrate = interestrate
         print("The Account is created")
 
-    def deposit(self,amount,):
+    def deposit(self,amount):
         amount = float(input("enter the amount to be deposit : "))
         self.balance = self.balance + amount
         print("Deposit is successful and the balance in the account is : ",self.balance )
 
     def withdrawal(self,amount):
-        amount = float(input("enter the amount to be withdrawa : "))
+        amount = float(input("enter the amount to be withdrawal : "))
         if (self.balance >= amount):
             self.balance = self.balance - amount
             print ("The withdrawal is successfull and balance id : ",self.balance)
@@ -26,11 +25,11 @@ class Account:
 
 
 class SavingAccount(Account):
-    def __init__(self,balance,interestrate):
-        super().__init__(balance)
-        self.interestRate = interestrate
+    def _init_(self,balance,interest_rate):
+        super()._init_(balance)
+        self.interest_rate = interest_rate
 
-    def interestrate(self,interestrate ,p,t,r):
+    def interestrate(self,interest_rate,p=0,t=0,r=0):
         p = float(input("enter the balance in account :"))
         t = int(input("enter the time period to keep :"))
         r = float(input("enter the interestrate :"))
@@ -38,12 +37,12 @@ class SavingAccount(Account):
         print("The time period in year is :", t)
         print("The rate of interest : ",r)
 
-        self.interestamount = (p * t * r)/100
-        print("interestamount is :",self.interestamount)
-        
-Account_obj = Account(1)
-Account_obj.deposit(1)
+        interest_rate = (p * t * r)/100
+        print("interestamount is :",interest_rate)
+        pass
+Account_obj = Account()
+Account_obj.deposit(1) 
 Account_obj.withdrawal(1)
 Account_obj.getBalance()
-obj = SavingAccount(Account)
-obj.interestRate(1)
+SavingAccount_obj=SavingAccount(balance=0,interest_rate=0)
+SavingAccount_obj.interest_rate(1)
